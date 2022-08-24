0a9148866e8c71d21a651d3d1d0ecf80319b3aa1
Lennard Schwarz <lennard.schwarz@elsanow.io>
2022-08-24 14:29:02 +0200

    change module export back to base dir

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-// this is necessary for gh pages to work.
-module.exports = {
-    publicPath: '/gefaehrliches-halbwissen/'
-}
+// this is necessary for gh pages to work with repository name. With custom domain, not necessary.
+// module.exports = {
+//     publicPath: '/gefaehrliches-halbwissen/'
+// }
